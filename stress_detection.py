96980fd2339e36284bed359758b846b859804c53
Stamatis Orfanos <stamatisorfanos99@gmail.com>
2025-07-09 11:42:19 +0700

    Changes to fix model performance

--- stress_detection.py
+++ stress_detection.py
@@ -95,7 +95,7 @@ if __name__ == '__main__':
     # Define models
     models = {
         'Logistic Regression':     LogisticRegression(max_iter=100, random_state=26, class_weight='balanced'),
-        'Random Forest':           RandomForestClassifier(n_estimators=10, random_state=26, min_samples_leaf=1, max_features='sqrt', class_weight='balanced'),
+        'Random Forest':           RandomForestClassifier(n_estimators=200, random_state=26, min_samples_leaf=1, max_features='sqrt', class_weight='balanced'),
         'SVM (Linear Kernel)':     SVC(kernel='linear', C=1.0, max_iter=10000, random_state=26, class_weight='balanced'),
         'SVM (Polynomial Kernel)': SVC(kernel='poly', C=1.0, max_iter=10000, random_state=26, class_weight='balanced'), 
         'SVM (RBF Kernel)':        SVC(kernel='rbf', C=1.0, max_iter=10000, random_state=26, class_weight='balanced'),  
